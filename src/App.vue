d1010bbe32535370f44dd432b24501ba2dba10e3
Nicowski25 <nicola.faedo0610@gmail.com>
2023-06-14 19:05:52 +0200

    app method axios

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
     components: {
-        HelloWorld,
+    },
+    data() {
+        return {
+            base_API: 'http://127.0.0.1:8000/',
+            projects_path: 'api/projects',
+            loading: true,
+            projects: null,
+            error: null,
+        }
+    },
+    methods: {
+        getProejcts(url) {
+            axios
+            .get(url)
+            .then(response => {
+                console.log(response);
+                this.projects = response.data.projects
+                this.loading = false
+            })
+            .catch(error => {
+                console.log(error);
+                this.error = error.message
+            })
+        }
+    },
+    mounted() {
+        const url = this.base_API + this.projects_path
+        this.getProejcts(url)
     }
 }
+
 </script>
 
 <template>
     <section class="vue-home">
         <div class="container">
+            <h1>Projects</h1>
             <div class="row">
-                <div class="col-12 d-flex flex-column justify-content-center align-items-center vh-100">
-                    <div class="logos">
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
-                    </div>
-                    <HelloWorld />
-                </div>
+            
             </div>
         </div>
     </section>
@@ -30,31 +49,4 @@ export default {
 
 <style lang="scss">
 @use './styles/general.scss';
-
-.vue-home {
-    color: #2c3e50;
-    background: #181818;
-    transition: color 0.5s, background-color 0.5s;
-    line-height: 1.6;
-    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-        Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-    font-size: 15px;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-}
-
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
