1d907253c7b675e6b8e5d098d9facc15a7907f71
Nicowski25 <nicola.faedo0610@gmail.com>
2023-09-11 15:02:34 +0200

    add 404 not found view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'NotFound',
+}
+</script>
+<template>
+    <div class="vh-100 d-flex flex-column align-items-center justify-content-center">
+        <h1 class="section-title mb-1 py-3">404 | Page Not Found</h1>
+        <router-link :to="{ name: 'home' }" class="btn btn-success mt-3">Back to Homepage</router-link>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
