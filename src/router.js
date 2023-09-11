6fa9a508b7c4247c4dc1d8943f7a644e38ee9dc8
Nicowski25 <nicola.faedo0610@gmail.com>
2023-09-11 15:08:07 +0200

    add new components and views

--- src/router.js
+++ src/router.js
@@ -1,31 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from './views/HomeView.vue';
 import AboutView from './views/AboutView.vue';
 import ContactsView from './views/ContactsView.vue';
 import ProjectsView from './views/ProjectsView.vue';
-import HomeView2 from './views/HomeView2.vue';
+import HomeView from './views/HomeView.vue';
+import NotFound from './views/NotFound.vue';
+import PrivacyView from './views/PrivacyView.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            'path' : '/',
-            'name' : 'home',
-            'component' : HomeView2
+            'path': '/',
+            'name': 'home',
+            'component': HomeView
         },
         {
-            'path' : '/about',
-            'name' : 'about',
-            'component' : AboutView
+            'path': '/about',
+            'name': 'about',
+            'component': AboutView
         },
         {
-            'path' : '/contacts',
-            'name' : 'contacts',
-            'component' : ContactsView
+            'path': '/contacts',
+            'name': 'contacts',
+            'component': ContactsView
         },
         {
-            'path' : '/projects',
-            'name' : 'projects',
-            'component' : ProjectsView
+            'path': '/projects',
+            'name': 'projects',
+            'component': ProjectsView
+        },
+        {
+            'path': '/:pathMatch(.*)*',
+            'name': 'NotFound',
+            'component': NotFound
+        },
+        {
+            'path': '/privacy',
+            'name': 'privacy',
+            'component': PrivacyView
         }
     ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,40 @@
 <script>
-import ProjectList from '../components/ProjectList.vue';
+import AppHeader from '../components/AppHeader.vue';
 import JumboHome from '../components/JumboHome.vue';
-import ProjectsView from './ProjectsView.vue';
+import ProjectList from '../components/ProjectList.vue';
+import LogoSlider from '../components/LogoSlider.vue';
+import SendEmail from '../components/SendEmail.vue';
+import AppFooter from '../components/AppFooter.vue';
 
 export default {
-    name: "HomeView",
+    name: "HomeView2",
     components: {
+        AppHeader,
         ProjectList,
         JumboHome,
-        ProjectsView
+        LogoSlider,
+        SendEmail,
+        AppFooter
     }
 }
 </script>
 
 <template>
-    <div class="d-flex flex-column">
-        <JumboHome></JumboHome>
-        <h1 class="text-center py-2">My Projects</h1>
-        <ProjectList></ProjectList>
-    </div>
+    <AppHeader></AppHeader>
+
+    <JumboHome></JumboHome>
+
+    <ProjectList></ProjectList>
+
+    <LogoSlider></LogoSlider>
+
+    <SendEmail></SendEmail>
+
+    <AppFooter></AppFooter>
 </template>
 
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 @use '../assets/scss/partials/commons' as *;
-
-h1 {
-    color: $yellow;
-    background-color: $black-light;
-}
-
-.bg-white {
-    background-color: $greywhite;
-}
-
 </style>
